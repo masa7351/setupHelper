cf86640f3af9bc83b84d50240c6793db9e40812a
Masa <masa7351@gmail.com>
2019-10-23 01:58:16 +0900

    Create README.md Remove comment out source

---
+++ README.md
@@ -0,0 +1,13 @@
+# setupHelper
+Support tools for xcode project creation.
+
+# Description
+This tool can create Podfile and install SwiftLint and SwiftFormat for your project.
+
+# Requirements & Support
+- Python 3.8.0
+
+# Example
+```
+python helper.py (your target_directory)
+```
--- helper.py
+++ helper.py
@@ -12,9 +12,6 @@ def podInit(path):
 
 # write library to Podfile
 def writeLibrary(path):
-#    swiftFormat = 'pod \'SwiftFormat/CLI\', \'~> 0.40.0\''
-#    swiftLint = 'pod \'SwiftLint\', \'~> 0.35.0\''
-#    text = swiftFormat + '\n' + swiftLint + '\n'
     insertText(path+'/Podfile', 3, insertPodLibrary)
 
 def insertText(path, insert_line, text):
@@ -23,7 +20,6 @@ def insertText(path, insert_line, text):
 
     if len(lines) > int(insert_line):
         lines.insert(2, text)
-#        lines[insert_line] = text
 
     with open(path, 'w') as file:
         file.writelines( lines )
