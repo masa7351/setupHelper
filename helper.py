f37be8a23bf2601741a303020f7b00b3c630e38c
Masanao Imai <masa7351@gmail.com>
2019-10-23 01:46:55 +0900

    first commit

---
+++ helper.py
@@ -0,0 +1,39 @@
+insertPodLibrary="""
+# Tools
+pod 'SwiftFormat/CLI', '~> 0.40.0'
+pod 'SwiftLint', '~> 0.35.0'
+"""
+
+# make Podfile
+def podInit(path):
+    import os
+    os.chdir(path)
+    os.system('pod init')
+
+# write library to Podfile
+def writeLibrary(path):
+#    swiftFormat = 'pod \'SwiftFormat/CLI\', \'~> 0.40.0\''
+#    swiftLint = 'pod \'SwiftLint\', \'~> 0.35.0\''
+#    text = swiftFormat + '\n' + swiftLint + '\n'
+    insertText(path+'/Podfile', 3, insertPodLibrary)
+
+def insertText(path, insert_line, text):
+    with open(path, 'r') as file:
+        lines = file.readlines()
+
+    if len(lines) > int(insert_line):
+        lines.insert(2, text)
+#        lines[insert_line] = text
+
+    with open(path, 'w') as file:
+        file.writelines( lines )
+
+def podInstall(path):
+    import os
+    os.system('pod install')
+
+if __name__ == "__main__":
+    import sys
+    podInit(sys.argv[1])
+    writeLibrary(sys.argv[1])
+    podInstall(sys.argv[1])
